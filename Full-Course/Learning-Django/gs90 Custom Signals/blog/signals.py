e14053639ae65443fb71469d52045a6bf30d63b0
noumanrehman042 <nouman.rehman@devsinc.com>
2024-01-14 22:53:49 +0500

    Learn Authentication and Signals

--- Full-Course/Learning-Django/gs90 Custom Signals/blog/signals.py
+++ Full-Course/Learning-Django/gs90 Custom Signals/blog/signals.py
@@ -1,11 +1,16 @@
 from django.dispatch import Signal, receiver
+
 # Creating Signals
-notification = Signal(providing_args=["request", "user"])
+# notification = Signal(providing_args=["request", "user"])
+notification = Signal()
 
 
-# Receiver Function
+# Receiver Function ( when notification signal is sent, our show_notification function will execute)
 @receiver(notification)
 def show_notification(sender, **kwargs):
     print(sender)
     print(f'{kwargs}')
     print("Notification")
+
+
+
--- Full-Course/Learning-Django/gs90 Custom Signals/blog/views.py
+++ Full-Course/Learning-Django/gs90 Custom Signals/blog/views.py
@@ -3,5 +3,6 @@ from blog import signals
 
 # Create your views here.
 def home(request):
-    signals.notification.send(sender=None, request=request, user = ['Geeky', 'Shows'])
+    signals.notification.send(sender=None, request=request, user=['Geeky', 'Shows'])
     return HttpResponse("This is Home Page")
+ 
